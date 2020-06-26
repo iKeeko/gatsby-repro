164022471ba1f737a6f52adcd0e8bf8d93f4b50a
Keeko <keeko.pdf@gmail.com>
2020-06-26 22:27:03 +1000

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,63 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
+    siteUrl: `https://www.gatsbyjs.org/`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Nunito`,
+            variants: [`200`, `300`, `400`, `600`, `700`],
+          },
+          {
+            family: `Montserrat`,
+            variants: [`300`, `400`],
+          },
+        ],
+      },
+    },
+    {
+      resolve: "gatsby-source-shopify",
+      options: {
+        shopName: "your-shop",
+        accessToken: "key",
+        verbose: true,
+        paginationSize: 100,
+      },
+    },
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-robots-txt`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://www.gatsbyjs.org/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-react-svg`,
+      options: {
+        rule: {
+          include: /svg/,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `#3ba53a`,
+        showSpinner: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: true,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -27,8 +81,25 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "google-analytics-id",
+        head: false,
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-mailchimp",
+      options: {
+        endpoint: `mailchimp-url`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-facebook-pixel`,
+      options: {
+        pixelId: "facebook-pixel-id",
+      },
+    },
   ],
 }
